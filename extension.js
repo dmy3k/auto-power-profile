ae89bfeec2b2102a06666a7da07986f22a2f8595
Dmytro K <3098166+dmy3k@users.noreply.github.com>
2025-03-26 13:05:37 +0100

    refactor extension.js (move notifier, profile transition to separate files)

--- extension.js
+++ extension.js
@@ -1,142 +1,18 @@
-import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 import UPower from "gi://UPowerGlib";
-import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
+import * as FileUtils from "resource:///org/gnome/shell/misc/fileUtils.js";
 import {
   Extension,
   gettext as _,
 } from "resource:///org/gnome/shell/extensions/extension.js";
-import * as FileUtils from "resource:///org/gnome/shell/misc/fileUtils.js";
-import * as MessageTray from "resource:///org/gnome/shell/ui/messageTray.js";
-import * as Config from "resource:///org/gnome/shell/misc/config.js";
 
+import { Notifier } from "./lib/notifier.js";
+import { ProfileTransition } from "./lib/profiletransition.js";
 import {
   createPowerProfilesProxy,
   createPowerManagerProxy,
 } from "./lib/utils.js";
-
-class Notifier {
-  constructor(extensionObject) {
-    this._uuid = extensionObject.uuid;
-    this._name = extensionObject.metadata.name;
-    this._source = null;
-  }
-
-  notify(body, uri) {
-    const [major] = Config.PACKAGE_VERSION.split(".");
-    const shellVersion45 = Number.parseInt(major) < 46;
-
-    const iconName = "dialog-warning-symbolic";
-    const title = _("Auto Power Profiles");
-    const urgency = MessageTray.Urgency.CRITICAL;
-
-    if (this._checkActiveNotification()) {
-      this._source.destroy(MessageTray.NotificationDestroyedReason.REPLACED);
-      this._source = null;
-    }
-
-    let notification;
-    if (shellVersion45) {
-      this._source = new MessageTray.Source(this._name, iconName);
-      notification = new MessageTray.Notification(this._source, title, body);
-      notification.setUrgency(urgency);
-      notification.setTransient(true);
-    } else {
-      this._source = new MessageTray.Source({
-        title: this._name,
-        icon: Gio.icon_new_for_string(iconName),
-      });
-      notification = new MessageTray.Notification({
-        source: this._source,
-        title,
-        body,
-        urgency,
-      });
-    }
-
-    Main.messageTray.add(this._source);
-
-    if (uri) {
-      notification.addAction(_("Show details"), () => {
-        Gio.app_info_launch_default_for_uri(uri, null, null, null);
-      });
-    }
-
-    if (shellVersion45) {
-      this._source.showNotification(notification);
-    } else {
-      this._source.addNotification(notification);
-    }
-  }
-
-  _checkActiveNotification() {
-    let status = false;
-    const activeSource = Main.messageTray.getSources();
-    if (activeSource[0] == null) {
-      this._source = null;
-    } else {
-      activeSource.forEach((item) => {
-        if (item === this._source) status = true;
-      });
-    }
-    return status;
-  }
-
-  _removeActiveNofications() {
-    if (this._checkActiveNotification())
-      this._source.destroy(
-        MessageTray.NotificationDestroyedReason.SOURCE_CLOSED
-      );
-    this._source = null;
-  }
-
-  destroy() {
-    this._removeActiveNofications();
-  }
-}
-
-class ProfileTransition {
-  effectiveProfile;
-  requestedProfile;
-  committedProfile;
-
-  onBat;
-  lowBat;
-
-  report({ effectiveProfile, onBattery, lowBattery }) {
-    this.effectiveProfile = effectiveProfile;
-    this.onBat = onBattery;
-    this.lowBat = lowBattery;
-
-    if (
-      this.requestedProfile &&
-      !this.committedProfile &&
-      this.effectiveProfile === this.requestedProfile
-    ) {
-      this.committedProfile = this.requestedProfile;
-    }
-
-    if (!effectiveProfile) {
-      this.effectiveProfile = null;
-      this.requestedProfile = null;
-      this.committedProfile = null;
-    }
-  }
-
-  request({ configuredProfile, onBattery, lowBattery }) {
-    const allowed =
-      this.lowBat !== lowBattery ||
-      this.onBat !== onBattery ||
-      !this.committedProfile;
-
-    if (allowed) {
-      this.requestedProfile = configuredProfile;
-      this.committedProfile = null;
-    }
-    return allowed;
-  }
-}
 
 export default class AutoPowerProfile extends Extension {
   _settings;
